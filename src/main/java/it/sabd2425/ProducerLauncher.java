e94ee0935b158a5534a6506df73c0684ce402ec1
GentiliEmanuele <egentili2000@gmail.com>
2025-06-24 16:42:57 +0200

    Added log message for debug

--- src/main/java/it/sabd2425/ProducerLauncher.java
+++ src/main/java/it/sabd2425/ProducerLauncher.java
@@ -8,7 +8,13 @@ import it.sabd2425.kafka.ChallengeProducer;
 import it.sabd2425.kafka.KafkaProducerFactory;
 import it.sabd2425.utils.Utils;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ProducerLauncher {
+
+    private static final Logger LOGGER = Logger.getLogger(ProducerLauncher.class.getName());
+
     public static void main(String[] args) throws DefaultApiException, InterruptedException {
         var benchConfig = Utils.toBenchConfig(StartBenchmarkCommand.fromArgs(args));
         var endpoint = Utils.getOrThrow("GC25_API_ENDPOINT");
@@ -24,6 +30,7 @@ public class ProducerLauncher {
                     break;
                 }
                 ++batchCount;
+                LOGGER.log(Level.INFO, String.format("retrieved successfully %d batches", batchCount));
                 producer.publishBatch(benchmark, o.get());
                 if (batchCount % batchSize == 0) {
                     producer.flush();
